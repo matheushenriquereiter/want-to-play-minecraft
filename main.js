64a1f0d94731242f94c17498a206a2f0cd7c0967
matheushenriquereiter <mathenriquereiter@gmail.com>
2023-10-26 15:02:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const noButton = document.querySelector(".no-button");
+const yesButton = document.querySelector(".yes-button");
+const container = document.querySelector(".container");
+const title = document.querySelector(".title");
+
+const moveButtonTo = (x, y) => {
+  noButton.style.left = `${x}px`;
+  noButton.style.top = `${y}px`;
+};
+
+noButton.addEventListener("click", () => {
+  const { innerWidth, innerHeight } = window;
+
+  const buttonWidth = noButton.offsetWidth;
+  const buttonHeight = noButton.offsetHeight;
+
+  let randomHeight = Math.trunc(Math.random() * innerHeight);
+  let randomWidth = Math.trunc(Math.random() * innerWidth);
+
+  const buttonIsCollideInRight = innerWidth - randomWidth < buttonWidth;
+  const buttonIsCollideInBottom = innerHeight - randomHeight < buttonHeight;
+
+  noButton.style.position = "absolute";
+
+  if (buttonIsCollideInRight) {
+    randomWidth -= buttonWidth;
+  }
+
+  if (buttonIsCollideInBottom) {
+    randomHeight -= buttonHeight;
+  }
+
+  moveButtonTo(randomWidth, randomHeight);
+});
+
+yesButton.addEventListener("click", () => {
+  title.textContent = "Só bora";
+  container.remove();
+});
